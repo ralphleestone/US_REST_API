c0add8c8cc753fb5458e4a1f6fe9f7053f86ba89
=Ralph Stone <ralphleestone@gmail.com>
2022-04-21 06:38:03 -0600

    added packages

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ console.log(path.dirname(_filename));
 console.log(path.basename(_filename));
 console.log(path.extname(_filename));
 
-console.log(path.parse(_filename));
+console.log(path.parse(_filename));exit
