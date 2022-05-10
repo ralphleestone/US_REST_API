1c9cee332118ef6206470e79928287e9742f58e2
Ralph Lee Stone <ralphleestone@gmail.com>
2022-05-10 15:22:56 -0600

    update

--- server.js
+++ server.js
@@ -6,12 +6,3 @@ require('dotenv').config();
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
-
-console.log(_dirname);
-console.log(_filename);
-
-console.log(path.dirname(_filename));
-console.log(path.basename(_filename));
-console.log(path.extname(_filename));
-
-console.log(path.parse(_filename));
