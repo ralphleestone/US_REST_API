8c7c52d574e6aa46ac02f70ab2247927167d006a
=Ralph Stone <ralphleestone@gmail.com>
2022-05-11 12:00:16 -0600

    update

--- routes/states.js
+++ routes/states.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
 
+/*
 router.route("/states/:state/funfact")
 .post(statesController.createStateInfo)
 //.patch(statesController.updateStateInfo);
-
-
-/*
-router.route('/:state')
-  .get(verifyState(), statesController.getOneState)
 */
+
+router.route('/:state')
+  .get(verifyState(), statesController.getAllStates)
