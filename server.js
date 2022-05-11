37c1a44724ba66448eef25352d7bd96af5e66857
=Ralph Stone <ralphleestone@gmail.com>
2022-05-11 10:22:58 -0600

    update

--- server.js
+++ server.js
@@ -4,27 +4,49 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-//const verifyState = require('./middleware/verifyState');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+//const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+//const credentials = require('./middleware/credentials');
 const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn')
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
 // Connect to MongoDB
 connectDB();
 
+// custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+//app.use(credentials);
+
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json 
 app.use(express.json());
 
+//middleware for cookies
+//app.use(cookieParser());
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
 // routes
 app.use('/', require('./routes/root'));
-//app.use('/states', require('./routes/states'));
-//app.use('/', require('./routes/states'));
+//app.use('/register', require('./routes/register'));
+//app.use('/auth', require('./routes/auth'));
+//app.use('/refresh', require('./routes/refresh'));
+//app.use('/logout', require('./routes/logout'));
 
-// built-in middleware to handle urlencoded form data
-app.use(express.urlencoded({ extended: false }));
+//app.use(verifyJWT);
+//app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
     res.status(404);
@@ -36,6 +58,8 @@ app.all('*', (req, res) => {
         res.type('txt').send("404 Not Found");
     }
 });
+
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
