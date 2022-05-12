a36971214d61d26ad2bf0cd61befcbf9ec03467c
=Ralph Stone <ralphleestone@gmail.com>
2022-05-11 21:46:35 -0600

    update

--- server.js
+++ server.js
@@ -1,46 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
+require("dotenv").config();
+const express = require("express");
+const errHandler = require("./middleware/errorHandler");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
 
-// Connect to MongoDB
+// Connect to DB Bellow
+
 connectDB();
 
-// built-in middleware for json 
+// middleware allowing use of JSON
 app.use(express.json());
 
 // cross origin resource sharing third-party middleware
 app.use(cors());
-
 // built in middleware
 app.use(express.urlencoded({ extended: false }));
 
-// Serve Static Files
-app.use('/', express.static(path.join(__dirname, '/public')));
+// middleware to serve static files
+app.use(express.static(path.join(__dirname, "/public")));
 
-// Routes
+// ----------------------------------------
+// Routes bellow
 app.use("/", require("./routes/root"));
 app.use("/states", require("./routes/api/states"));
 
 app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-        res.json({ error: "404 not Found" });
-    } else {
-        res.type("txt").send("404 Not Found");
-    }
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
-app.use(errorHandler);
+app.use(errHandler);
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
